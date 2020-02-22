ab1a1a30cfbf272c4bdf8fc388b2dab127a76f75
Ericwyn <Ericwyn.chen@gmail.com>
2020-02-23 00:15:51 +0800

    add cnki pdf download script
    添加 cnki pdf 下载脚本（原理是国外版本的CNKI支持pdf下载，所以国内只是屏蔽了按钮而已）

---
+++ cnkiAddPDFDownload.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         CNKI-PDF下载
+// @namespace    http://*.gxlib.org/KCMS/detail/detail.aspx?*
+// @version      0.1
+// @description  显示 CNKI-PDF 下载按钮
+// @author       Ericwyn (github.com/Ericwyn)
+// @match        *.gxlib.org/KCMS/detail/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    window.addPDFDownload = function(){
+        if(document.getElementById("DownLoadParts") != undefined) {
+            if(document.getElementById("DownLoadParts").outerHTML.indexOf("PDF下载") >= 0) {
+                return
+            }
+            let inner = document.getElementById("DownLoadParts").children[0].outerHTML
+            if(inner.indexOf("dflag=nhdown")>0 && inner.indexOf("整本下载")>0 ){
+                inner = inner.replace("dflag=nhdown","dflag=pdfdown")
+                inner = inner.replace("整本下载","PDF全文下载")
+                document.getElementById("DownLoadParts").innerHTML += inner
+            }
+        }
+    }
+    window.addPDFDownload();
+})();
